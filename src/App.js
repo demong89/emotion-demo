d0a960a5330995e8e63f7d69ec010f7db9f88371
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2021-08-05 16:14:05 +0800

    feat: eject config

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p css={{width:100,height:100,background:'red'}}>
+          src/App.js
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
           Learn React
-        </a>
+       
       </header>
     </div>
   );
